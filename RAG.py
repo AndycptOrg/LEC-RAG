fa37f23bf93725f2e51e16413e0ba31606313e7e
andy <andycpt20@outlook.com>
2026-06-22 16:32:21 +0100

    moved import statements and general clean up

--- RAG.py
+++ RAG.py
@@ -1,9 +1,14 @@
 import faiss
 from transformers.utils.logging import disable_progress_bar
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+import torch
 disable_progress_bar()
 
+from corpus import get_model, get_embedding, get_docs
+
 def search(model, index, queries, k):
-    from corpus import documents
+    documents = get_docs()
+
     # 5. Execute a Semantic Search
     query_vector = model.encode(queries).astype('float32')
     faiss.normalize_L2(query_vector)
@@ -14,20 +19,18 @@ def search(model, index, queries, k):
     return [zip(similarities[i], [documents[j] for j in indices[i]]) for i in range(len(queries))]
 
 def query(query, k=10, model_index=None, debug=False):
-
-    # import ollama
     if model_index is None:
-        from corpus import model, get_embedding
         index = get_embedding()
+        encoding_model = get_model()
     else:
-        model, index = model_index
+        encoding_model, index = model_index
 
-    docs = search(model, index, [query], k)[0]
+    docs = search(encoding_model, index, [query], k)[0]
     
     # feed query with docs to the LLM for answer generation
-    context = "" #f"Answer the following question based on the provided documents:\n\nQuestion: {query}\n\nDocuments:\n"
+    context = ""
     for score, doc in docs:
-        context += f"{doc} "# (Score: {score:.4f})\n"
+        context += f"{doc} "
         if (debug):
             print(score)
     
@@ -35,23 +38,20 @@ def query(query, k=10, model_index=None, debug=False):
         print(query, context)
     
     # V6 slimmed local
-    from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
-    import torch
-
     LLM_MODEL = "google/flan-t5-base"#"Qwen/Qwen2.5-3B-Instruct"/"HuggingFaceTB/SmolLM3-3B"
     # TODO: migrate to CasualLM model: summarasation -> rerank -> CasualLM
     tokenizer = AutoTokenizer.from_pretrained(LLM_MODEL, tqdm_class=None)
-    model = AutoModelForSeq2SeqLM.from_pretrained(LLM_MODEL, tqdm_class=None)
+    llm_model = AutoModelForSeq2SeqLM.from_pretrained(LLM_MODEL, tqdm_class=None)
     
     prompt = f"{context}\n Question: {query}\n Answer:"
 
     inputs = tokenizer(prompt, return_tensors="pt", truncation=True, max_length=512)
     
     with torch.no_grad():
-        output_tokens = model.generate(
+        output_tokens = llm_model.generate(
             **inputs, 
             max_new_tokens=256,
-            temperature=0.7,
+            temperature=0.5,
             do_sample=True
         )
 
@@ -61,17 +61,15 @@ def query(query, k=10, model_index=None, debug=False):
     # 5. Decode the newly created tokens back to human text
     response = tokenizer.decode(output_tokens[0], skip_special_tokens=True)
 
-    # response = ollama.generate(model="llama3", prompt=prompt)
     return response.strip() #['answer']
 
 
 if __name__ == "__main__":
     # models for extracting corpus
-    from corpus import model, get_embedding
     index = get_embedding()
 
     question = "Tell me about AI and neural networks"
     question = "How do you set up ssh?"
-    answer = query(question, k=3, model_index=(model, index), debug=True)
+    answer = query(question, k=3, model_index=(get_model(), index), debug=True)
     print("Question:", question)
     print("Answer:", answer)
--- corpus.py
+++ corpus.py
@@ -1,21 +1,30 @@
+from pathlib import Path
+import csv
 from sentence_transformers import SentenceTransformer
+import faiss
+import numpy as np
 # 1. Load the embedding model
 model = SentenceTransformer('all-MiniLM-L6-v2')
-documents = []
+documents = [] # TODO: singleton
+
+def get_model():
+    return model
+
+CORPUS_PATH = Path('.') / 'synthetic-it-related-knowledge-items' / 'synthetic_knowledge_items.csv'
 
 def get_docs():
-    import csv
-
-    with open('synthetic-it-related-knowledge-items\synthetic_knowledge_items.csv', 'r', newline='', encoding='utf-8') as file:
-        reader = csv.DictReader(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        first_column = [row['ki_text']for row in reader]
-        documents.extend(first_column)
-    return first_column
+    if len(documents) == 0:
+        
+        with CORPUS_PATH.open('r', newline='', encoding='utf-8') as file:
+            reader = csv.DictReader(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            first_column = [row['ki_text']for row in reader]
+            documents.extend(first_column)
+        return first_column
+    else:
+        return documents
 
 def embed_docs(docs):
     # returns faiss index
-    import faiss
-    import numpy as np  
 
     # 3. Generate embeddings and convert to float32 (required by FAISS)
     document_embeddings = model.encode(docs)
@@ -33,15 +42,12 @@ def embed_docs(docs):
 INDEX_NAME = "embedded_docs.index"
 
 def save_embedding(index):
-    import faiss
     faiss.write_index(index, INDEX_NAME)
 
 def get_embedding():
-    import os
-    index_file = os.path.join(os.path.dirname(__file__), INDEX_NAME)
+    index_file = Path('.') / INDEX_NAME
 
-    if (os.path.exists(index_file)):
-        import faiss
+    if (index_file.exists()):
         index = faiss.read_index(INDEX_NAME)
         get_docs()
     else:
